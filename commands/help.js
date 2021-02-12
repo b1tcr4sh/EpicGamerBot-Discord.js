3cc0927d376954f9108d8bc61f80227973e69057
Ethan Ford <ethan.ford06@gmail.com>
2021-02-12 11:25:33 -0600

    Added 'Help Admin' Arguments

--- commands/help.js
+++ commands/help.js
@@ -2,11 +2,8 @@
 module.exports = {
     name: 'help',
     description: "Displays all command aliases and descriptions.",
-    execute(message, args, client, commandFiles) {
-        if (args[0] === 'admin' && message.member.roles.cache.has('738215800778784859')) {
-            
-        }
-        else {
+    execute(message, args, client, commandFiles, staffCommandFiles) {
+        if (!args.lenth) {
             message.channel.send('Currently Supported Commands:');
             commandFiles.forEach(element => {
                 let modifiedElement = element.substring(0, element.length-3);
@@ -15,6 +12,20 @@ module.exports = {
                 let description = client.commands.get(modifiedElement).description;
                 message.channel.send(`- ${name}: ${description}`);
             }); 
+        }
+        else if (args[0] === 'admin') {
+            if (message.member.roles.cache.has('738215800778784859')) {
+                message.channel.send('Currently Supported Staff Commands:');
+                staffCommandFiles.forEach(element => {
+                    let modifiedElement = element.substring(0, element.length-3);
+
+                    let name = client.commands.get(modifiedElement).name;
+                    let description = client.commands.get(modifiedElement).description;
+                    message.channel.send(`- ${name}: ${description}`);
+                });
+            } else {
+                message.reply('You do not have sufficient permissions to perform this comand!');
+            }
         }
     }
 }
