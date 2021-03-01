20bda7f60b2cfe286be3a690dcb692accd47b415
Ethan Ford <ethan.ford06@gmail.com>
2021-03-01 11:13:23 -0600

    Added switch method for determining user roles - Code Refactiring

--- commands/staff/jail.js
+++ commands/staff/jail.js
@@ -1,10 +1,14 @@
-const { User, Role } = require("discord.js");
+const { User, Role, ReactionUserManager } = require("discord.js");
 
 module.exports = {
     name: 'jail',
     description: 'Banishes a user to the horny gulag',
     execute(message, args) {
         const mutedRole = message.guild.roles.cache.find(role => role.name === "Certified Simp");
+        const smallEpicGamerRole = message.guild.roles.cache.find(role => role.name === "Small Epic Gamer");
+        const epicGamerRole = message.guild.roles.cache.find(role => role.name === "Epic Gamer");
+        const bigEpicGamerRole = message.guild.roles.cache.find(role => role.name === "Big Epic Gamer");
+        const epicGamerBroskisRole = message.guild.roles.cache.find(role => role.name === "Epic Gamer Broskis")
         const {member, mentions} = message
         
         try {
@@ -12,11 +16,27 @@ module.exports = {
                 const target = message.mentions.users.first();
                 let targetMember = message.guild.members.cache.get(target.id);
     
-                if (!targetMember.roles.cache.has(mutedRole)) {
-                    targetMember.roles.add(mutedRole);
-                } else {
-                    targetMember.roles.remove(mutedRole);
-                }
+                    switch (targetMember.roles.cache.has()) {
+                        case smallEpicGamerRole:
+                            targetMember.roles.remove(smallEpicGamerRole);
+                            return;
+                        case epicGamerRole:
+                            targetMember.roles.remove(epicGamerRole);
+                            return;
+                        case bigEpicGamerRole:
+                            targetMember.roles.remove(bigEpicGamerRole);
+                            return;
+                        case epicGamerRole:
+                            targetMember.roles.remove(epicGamerRole);
+                            return;
+                        case epicGamerBroskisRole:
+                            targetMember.roles.remove(epicGamerBroskisRole);
+                            return;
+                        default:
+                            targetMember.roles.remove(mutedRole);
+                            targetMember.roles.add(smallEpicGamerRole);
+                            break;
+                    }
             } else {
                 throw 'You do not have sufficient permissions to perform this command!';
             }
