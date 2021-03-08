5ccf50c78db0b5968c5c34b9ca17663fa5b485af
Ethan Ford <ethan.ford06@gmail.com>
2021-03-08 14:04:54 -0600

    Made Changes

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,8 @@ module.exports = {
     name: 'help',
     description: "Displays all command aliases and descriptions.",
     execute(message, args, client, commandFiles, staffCommandFiles, Discord) {
+        const supportedCommands = client.commands;
+
         const helpEmbed = new Discord.MessageEmbed()
         .setTitle('Currently Supported Commands:')
 
@@ -20,25 +22,30 @@ module.exports = {
             }); 
             message.channel.send(helpEmbed);
         }
+        else if (args[0] === supportedCommands) {
+            let name = client.commands.get(args[0]).name;
+            let description = client.commands.get(args[0]).description;
+
+            helpEmbed.addFields(
+                {name: name, value: description}
+            );
+        }
         else if (args[0] === 'admin') {
-            if (message.member.roles.cache.has('738215800778784859')) {
-                helpEmbed.setColor('#FF502B')
-                .setDescription('Admin Specific Commands');                    
+            if (!message.member.roles.cache.has('738215800778784859')) return message.reply('You do not have sufficient permissions to perform this command');
+                
+            helpEmbed.setColor('#FF502B')
+            .setDescription('Admin Specific Commands');                    
 
-                staffCommandFiles.forEach(element => {
-                    let modifiedElement = element.substring(0, element.length-3);
+            staffCommandFiles.forEach(element => {
+                let modifiedElement = element.substring(0, element.length-3);
 
-                    let name = client.commands.get(modifiedElement).name;
-                    let description = client.commands.get(modifiedElement).description;
-                    helpEmbed.addFields(
-                        {name: name, value: description}
-                    );
-                });
-                message.channel.send(helpEmbed);
-            } 
-            else {
-                message.reply('You do not have sufficient permissions to perform this comand!');
-            }
+                let name = client.commands.get(modifiedElement).name;
+                let description = client.commands.get(modifiedElement).description;
+                helpEmbed.addFields(
+                    {name: name, value: description}
+                );
+             });
+            message.channel.send(helpEmbed);
         }
     }
 }
