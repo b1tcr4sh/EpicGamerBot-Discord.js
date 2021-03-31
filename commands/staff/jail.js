5bbe6351237c5d1caaba0ecc3869e1e89fc339d5
Ethan Ford <ethan.ford06@gmail.com>
2021-03-30 19:35:30 -0500

    Fixed Jail Command, code is messy, will fix in the future

--- commands/staff/jail.js
+++ commands/staff/jail.js
@@ -4,44 +4,31 @@ module.exports = {
     name: 'jail',
     description: 'Banishes a user to the horny gulag',
     execute(message, args) {
-        const mutedRole = message.guild.roles.cache.find(role => role.name === "Certified Simp");
-        const smallEpicGamerRole = message.guild.roles.cache.find(role => role.name === "Small Epic Gamer");
-        const epicGamerRole = message.guild.roles.cache.find(role => role.name === "Epic Gamer");
-        const bigEpicGamerRole = message.guild.roles.cache.find(role => role.name === "Big Epic Gamer");
-        const epicGamerBroskisRole = message.guild.roles.cache.find(role => role.name === "Epic Gamer Broskis")
-        const {member, mentions} = message
+        const mutedRole = "754154227730743337";
+        const smallEpicGamerRole = "738215330027143189";
+        const epicGamerRole = "738470317001015386";
+        const bigEpicGamerRole = "738215400545976331";
+        const epicGamerBroskisRole = "738470506465853520";
+        const roles = ["738215330027143189", "738470317001015386", "738215400545976331", "738470506465853520"];
+        const {member, mentions} = message;
         
-        try {
-            if (member.roles.cache.has('738215800778784859')) {
-                const target = message.mentions.users.first();
-                let targetMember = message.guild.members.cache.get(target.id);
-    
-                    switch (targetMember.roles.cache.has()) {
-                        case smallEpicGamerRole:
-                            targetMember.roles.remove(smallEpicGamerRole);
-                            return;
-                        case epicGamerRole:
-                            targetMember.roles.remove(epicGamerRole);
-                            return;
-                        case bigEpicGamerRole:
-                            targetMember.roles.remove(bigEpicGamerRole);
-                            return;
-                        case epicGamerRole:
-                            targetMember.roles.remove(epicGamerRole);
-                            return;
-                        case epicGamerBroskisRole:
-                            targetMember.roles.remove(epicGamerBroskisRole);
-                            return;
-                        default:
-                            targetMember.roles.remove(mutedRole);
-                            targetMember.roles.add(smallEpicGamerRole);
-                            break;
-                    }
-            } else {
-                throw 'You do not have sufficient permissions to perform this command!';
-            }
-        } catch(error) {
-            message.channel.send(error);
+        const target = message.mentions.users.first();
+        let targetMember = message.guild.members.cache.get(target.id);
+        let targetMemberRoles = targetMember._roles;
+
+        if (targetMemberRoles.includes(mutedRole)) {
+            targetMember.roles.remove(mutedRole);
+            targetMember.roles.add(smallEpicGamerRole);
+
+            message.reply(`${target.name} has been successfully unmuted!`);
+        } else {
+            targetMember.roles.remove(smallEpicGamerRole);
+            targetMember.roles.remove(epicGamerRole);
+            targetMember.roles.remove(bigEpicGamerRole);
+            targetMember.roles.remove(epicGamerBroskisRole);
+            targetMember.roles.add(mutedRole);
+
+            message.reply(`${target.name} has been successfully muted!`);
         }
     }
 }
