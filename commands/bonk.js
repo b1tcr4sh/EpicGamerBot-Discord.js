3da95cfe1edf1fe583e4b0e903af40e47b5e675a
Vivid <74485747+TheArcticHusky@users.noreply.github.com>
2021-04-07 17:59:13 -0500

    Updated messages
    Changed one message to make more appropriate.

--- commands/bonk.js
+++ commands/bonk.js
@@ -5,7 +5,7 @@ module.exports = {
         let target = message.mentions.users.first();
         let targetMember = message.guild.members.cache.get(target.id);
         const bonkEmoji = client.emojis.cache.find(emoji => emoji.name === 'Bonk');
-        const messages = [`Bonked ${targetMember} ${bonkEmoji}`, `${targetMember} Get bonked child`, `${targetMember}, Get fucked by ${message.author} with a bat`, `Bonk! ${targetMember}, go to horny jail! ${bonkEmoji}`];
+        const messages = [`Bonked ${targetMember} ${bonkEmoji}`, `${targetMember} Get bonked child`, `${targetMember}, Get smacked by ${message.author} with a bat`, `Bonk! ${targetMember}, go to horny jail! ${bonkEmoji}`];
         
         let randomMessage = messages[Math.floor(Math.random() * messages.length)]
         message.channel.send(randomMessage);
