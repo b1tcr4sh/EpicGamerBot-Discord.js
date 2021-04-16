1f6de24d02e14e1f65f005ad538af8bc04fdb204
Ethan Ford <ethan.ford06@gmail.com>
2021-04-16 11:06:29 -0500

    Refactored All Command Arguments
    Refactored all commands with arguments so that all arguments exist as separate member  function of the parent object, being called from 'execute()' when invoked.

--- commands/staff/log.js
+++ commands/staff/log.js
@@ -4,20 +4,28 @@ module.exports = {
     name: 'log',
     description: "An array of commands for admins to interact with bot logs.",
     execute(message, args) {
-        const dateObj = new Date();
-        const seconds = dateObj.getSeconds();
 
         if (!message.member.roles.cache.has('738215800778784859')) return message.reply('You have insufficient permissions to perform this command!');
 
         if (args[0] === "view") {
-            message.channel.send({ files: ["./recentlog.txt"]});
+            this.view(message);
         }
         else if (args[0] === "clear") {
-            fs.writeFile('recentLog.txt', `[ ${seconds} ]s user: ${message.author} cleared the log \r\n`, function (err) {
-                if (err) return console.log(err);
-            });
-            message.channel.send('Log Cleared!');
+            this.clear(message);
         }
         else return message.channel.send('This command requires arguments!');
+    },
+    view(message) {
+        message.channel.send({ files: ['./recentLog.txt']});
+        console.log('Synchronous File Upload Started; Uploading log file');
+    },
+    clear(message) {
+        const dateObj = new Date();
+        const seconds = dateObj.getSeconds();
+
+        fs.writeFile('recentLog.txt', `[ ${seconds} ]s user: ${message.author} cleared the log \r\n`, (err) => {
+            if (err) return console.log(err);
+        });
+        message.channel.send('Log Cleared!');
     }
 }
