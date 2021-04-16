6e7bd94fa822f90be32561caaba20a124809a150
Ethan Ford <ethan.ford06@gmail.com>
2021-04-16 11:20:47 -0500

    Refactored Help Command

--- commands/help.js
+++ commands/help.js
@@ -16,7 +16,18 @@ module.exports = {
         const helpEmbed = new Discord.MessageEmbed()
 
         if (args[0] === undefined) {
-            helpEmbed.setTitle('Currently Supported Commands:')
+            this.empty(message, helpEmbed, commandFiles);
+        }
+        else if (args[0] === 'admin') {
+            this.admin(message, helpEmbed, staffCommandFiles);   
+        }
+        else if (supportedCommands.includes(argsFirst)) {
+            this.anyCommand(message, helpEmbed, argsFirst)
+        }
+        else message.reply("Unrecognized Argument");
+    },
+    empty(message, helpEmbed, commandFiles) {
+        helpEmbed.setTitle('Currently Supported Commands:')
             .setColor('#2BFF78');
 
             commandFiles.forEach(element => {
@@ -30,51 +41,49 @@ module.exports = {
                 });
             }); 
             message.channel.send(helpEmbed);
-        }
-        else if (args[0] === 'admin') {
-            if (!message.member.roles.cache.has('738215800778784859')) return message.reply('You do not have sufficient permissions to perform this command');
+    },
+    admin(message, helpEmbed, staffCommandFiles) {
+        if (!message.member.roles.cache.has('738215800778784859')) return message.reply('You do not have sufficient permissions to perform this command');
                 
-            helpEmbed.setTitle('Currently Supported Commands:')
-            .setColor('#FF502B')
-            .setDescription('Admin Specific Commands');                    
+        helpEmbed.setTitle('Currently Supported Commands:')
+        .setColor('#FF502B')
+        .setDescription('Admin Specific Commands');                    
 
-            staffCommandFiles.forEach(element => {
-                let modifiedElement = element.substring(0, element.length-3);
+        staffCommandFiles.forEach(element => {
+            let modifiedElement = element.substring(0, element.length-3);
 
-                let name = client.commands.get(modifiedElement).name;
-                let description = client.commands.get(modifiedElement).description;
-                helpEmbed.addFields({
-                    name: name,
-                    value: description
-                });
-             });
-            message.channel.send(helpEmbed);
-        }
-        else if (supportedCommands.includes(argsFirst)) {
-            let commandObject = client.commands.get(argsFirst);
-
-            let name = client.commands.get(args[0]).name;
-            let description = client.commands.get(args[0]).description;
-            let commandMethods = Object.getOwnPropertyNames(commandObject).filter(element => {
-                return typeof commandObject[element] === 'function';
-            })
-
-            let commandArgs = commandMethods.filter(method => method !== 'execute');
-            console.log(commandArgs);
-            if (!commandArgs.length) {
-                commandArgs = 'This Command Has No Arguments!';
-            }
-
-            helpEmbed.setTitle(name)
-            .setColor('#2BFF78')
-            .setDescription(description)
-            .addFields({
-                name: 'Available Arguments:', 
-                value: commandArgs
+            let name = client.commands.get(modifiedElement).name;
+            let description = client.commands.get(modifiedElement).description;
+            helpEmbed.addFields({
+                name: name,
+                value: description
             });
+         });
+        message.channel.send(helpEmbed);
+    },
+    anyCommand(message, helpEmbed, argsFirst) {
+        let commandObject = client.commands.get(argsFirst);
 
-            message.channel.send(helpEmbed);
+        let name = client.commands.get(args[0]).name;
+        let description = client.commands.get(args[0]).description;
+        let commandMethods = Object.getOwnPropertyNames(commandObject).filter(element => {
+            return typeof commandObject[element] === 'function';
+        })
+
+        let commandArgs = commandMethods.filter(method => method !== 'execute');
+        
+        if (!commandArgs.length) {
+            commandArgs = 'This Command Has No Arguments!';
         }
-        else message.reply("Unrecognized Argument");
+
+        helpEmbed.setTitle(name)
+        .setColor('#2BFF78')
+        .setDescription(description)
+        .addFields({
+            name: 'Available Arguments:', 
+            value: commandArgs
+        });
+
+        message.channel.send(helpEmbed);
     }
 }
