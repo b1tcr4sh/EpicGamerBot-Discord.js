4507a232e2b0f974816986066dee357115fe042a
Ethan Ford <ethan.ford06@gmail.com>
2021-04-17 23:04:28 -0500

    Refactored Argument Methods

--- commands/staff/sudo.js
+++ commands/staff/sudo.js
@@ -3,11 +3,22 @@ const { DiscordAPIError } = require("discord.js");
 module.exports = {
     name: 'sudo',
     description: "Writes specified message under bot name.",
-    async execute(message, args, client, commandFiles, staffCommandFiles, Discord, config){
-
+    execute(message, args, client, commandFiles, staffCommandFiles, Discord, config){
         if (!args.length) return message.channel.send('This command requires arguments!');
+        
         else if (args[0] === 'embed') {
-            const customEmbed = new Discord.MessageEmbed();
+            this.embed(message, args, client, commandFiles, staffCommandFiles, Discord, config);
+        }
+        else {
+            this.empty(message);
+        }
+    },
+    empty(message) {
+        message.delete();
+        message.channel.send(args.join(' '));
+    },
+    embed(message, args, client, commandFiles, staffCommandFiles, Discord, config ) {
+        const customEmbed = new Discord.MessageEmbed();
             message.channel.send('Enter the embed title:');
 
             const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {time: 10000}); 
@@ -18,11 +29,5 @@ module.exports = {
 
             const title = global.input;
             console.log(title);
-
-        }
-        else {
-            message.delete();
-            message.channel.send(args.join(' '));
-        }
     }
 }
