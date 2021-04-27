5f6873b12d9f003b9d1a75cd1b413eed44f73cd7
Ethan Ford <ethan.ford06@gmail.com>
2021-04-27 18:46:25 -0500

    Added error handler to ping command

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,12 @@ module.exports = {
     description: "Displays bot online status.",
     permissions: "User",
     disabled: false,
-    execute(message, args){
-        message.channel.send('All systems working!');
+    execute(message, args, client, commandFiles, staffCommandFiles, Discord, config, version){
+        message.channel.send('All systems working!')
+        .then(() => console.log(`Received ping from ${message.author}, bot is responsive.`));
+        .catch(error => {
+            client.users.cache.get('219273969415487489').send(`Ecountered Critial Exception: ${error}`); 
+            console.error(error);
+        })
     }
 }
