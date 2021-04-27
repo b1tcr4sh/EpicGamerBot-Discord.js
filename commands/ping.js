7bdf409ed7795763987d6bc55336d277e2c5a48f
Ethan Ford <ethan.ford06@gmail.com>
2021-04-27 18:46:55 -0500

    Fixed minor bug

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
     disabled: false,
     execute(message, args, client, commandFiles, staffCommandFiles, Discord, config, version){
         message.channel.send('All systems working!')
-        .then(() => console.log(`Received ping from ${message.author}, bot is responsive.`));
+        .then(() => console.log(`Received ping from ${message.author}, bot is responsive.`))
         .catch(error => {
             client.users.cache.get('219273969415487489').send(`Ecountered Critial Exception: ${error}`); 
             console.error(error);
