b16547645ef88b02aec0db3781923b862749ffd7
Ethan Ford <ethan.ford06@gmail.com>
2021-04-27 21:12:44 -0500

    Removed 'setPrefix' arg, was impossible and uneeded

--- commands/staff/config.js
+++ commands/staff/config.js
@@ -22,24 +22,5 @@ module.exports = {
         })
         .then(() => message.reply('Client Status Reinitialized!'))
         .catch(error => console.error(error));
-    },
-    async setPrefix(client, message, version) {
-        let config = require('../../config.json');
-
-        message.channel.send('Please enter the character(s) you would like to set as the prefix (Type "cancel" to cancel');
-
-        const messageFilter = m => m.author === message.author;
-        message.channel.awaitMessages(messageFilter, {max: 1, time: 10000, errors: ['time']})
-        .then(collected => {
-            let prefix = collected.first().content; 
-
-            if (prefix === 'cancel') return message.channel.send('Operation canceled');
-
-            config.prefix = prefix.toString();
-            console.log(`${prefix} has been set as the new prefix by ${message.author}`);
-            message.channel.send(`Bot prefix has successfully been set as ${prefix}`);
-
-            this.initializeStatus(client, message, config, version);
-        })
     }
 }
