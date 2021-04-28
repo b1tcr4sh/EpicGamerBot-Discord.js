fb61bf690e3afcc183d880789f17cc9a439369f3
Ethan Ford <ethan.ford06@gmail.com>
2021-04-27 21:34:30 -0500

    Added 'setStatus' argument

--- commands/staff/config.js
+++ commands/staff/config.js
@@ -8,19 +8,36 @@ module.exports = {
             case 'initializeStatus':
                 this.initializeStatus(client, message, config, version);
             break;
-            case 'setPrefix':
-                this.setPrefix(client, message, version);
+            case 'setStatus':
+                this.setStatus(client, message, version);
                 break;
             default:
                 message.reply('Unrecognized Argument!');
         }
     },
     initializeStatus(client, message, config, version) {
-        client.user.setActivity(`${config.prefix}help | v${version}`, {
-            type: "LISTENING",
-            url: "https://github.com/TheArcticHusky/EpicGamerBot-Discord.js"
-        })
+        client.user.setActivity(`${config.prefix}help | v${version}`, {type: "LISTENING"})
         .then(() => message.reply('Client Status Reinitialized!'))
         .catch(error => console.error(error));
+    },
+    async setStatus(client, message, version) {
+
+        let instructions = await message.channel.send('Please enter the message to overwrite the current status ("cancel" to cancel to operation)');
+
+        const messageFilter = m => m.author === message.author;
+        message.channel.awaitMessages(messageFilter, {max: 1, time: 10000, errors: ['time']})
+        .then(collected => {
+            let status = collected.first()
+
+            if (status.content === 'cancel') return message.channel.send('Operation canceled');
+            
+            
+            client.user.setActivity(status.content, {type: "PLAYING"})
+            .then(() => {
+                instructions.delete();
+                status.delete();
+                message.reply(`The client status has been set to ${status.content}`)
+            });
+        })
     }
 }
